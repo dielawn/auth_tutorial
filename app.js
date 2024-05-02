0f2beba179a41f2737deb8d33337e91e1b8a82dc
dielawn <dielawnmerkill@gmail.com>
2024-05-02 06:03:08 -0600

    set up environmental variables sign up route

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+require('dotenv').config();
 
-const mongoDb = "YOUR MONGO URL HERE";
+const mongoDb = process.env.MONGODB_URI
 mongoose.connect(mongoDb);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
