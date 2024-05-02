725b0b2ae5d1a7d31a55f82a61c86e4eb25f6c88
dielawn <dielawnmerkill@gmail.com>
2024-05-02 06:18:57 -0600

    user password auth middleware function

--- app.js
+++ app.js
@@ -44,5 +44,22 @@ app.post('/sign-up', async (req, res, next) => {
     };
 });
 
-app.listen(3000, () => console.log('app listening on port 3000!'));
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username: username });
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username' });
+            };
+            if (user.password !== password) {
+                return done(null, false, { message: 'Incorrect password' });
+            };
+            return done(null, user);
+        } catch(err) {
+            return done(err);
+        };
+    })
+);
+
+app.listen(3000, () => console.log('app listening on port 3000! http://localhost:3000/'));
 
