bb979630cf7f28a70da3be0d0ba29b5ad46925f9
dielawn <dielawnmerkill@gmail.com>
2024-05-03 05:55:43 -0600

    redirects to log-in or sign-up

--- README.md
+++ README.md
@@ -5,10 +5,14 @@
 Create/nav to project dir
 
 Create package.json
-``` npm init ```
+```npm init```
 
 Install dependencies
-``` npm install express express-session mongoose passport passport-local ejs ```
+```npm install express express-session mongoose passport passport-local ejs ```
 
 
+Secure passwords w/ bcrypt
+```npm install bcryptjs```
 
+app.js
+```require('/bcryptjs');```
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 require('dotenv').config();
+const bcrypt = require('bcryptjs');
 
 const mongoDb = process.env.MONGODB_URI
 mongoose.connect(mongoDb);
@@ -28,6 +29,13 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+//currentUser 
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+  });
+  
+
 app.get("/", (req, res) => {
     res.render("index", { user: req.user });
 });
@@ -37,9 +45,11 @@ app.get('/sign-up', (req,res) => {
 });
 app.post('/sign-up', async (req, res, next) => {
     try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
         const user = new User({
             username: req.body.username,
-            password: req.body.password,
+            password: hashedPassword,
         });
         await user.save();
         res.redirect('/');
@@ -51,11 +61,14 @@ app.post('/sign-up', async (req, res, next) => {
 passport.use(
     new LocalStrategy(async (username, password, done) => {
         try {
+
             const user = await User.findOne({ username: username });
             if (!user) {
                 return done(null, false, { message: 'Incorrect username' });
             };
-            if (user.password !== password) {
+
+            const match = await bcrypt.compare(password, user.password)
+            if (!match) {
                 return done(null, false, { message: 'Incorrect password' });
             };
             return done(null, user);
@@ -77,6 +90,8 @@ passport.deserializeUser(async (id, done) => {
         done(err);
     };
 });
+
+
 
 app.post(
     '/log-in',
